82c4650ca21561043710feb69b5eeb98fcf1a718
JoshyyCoded <largadojoshhunter@gmail.com>
2026-02-08 13:09:22 +0800

    Calculator.cs

--- Calculator.cs
+++ Calculator.cs
@@ -1,12 +1,42 @@
 using System;
 
-public static class Calculator
+class Calculator
 {
     public static double? Calculate(int first, int second, string operation)
     {
-        //write your business logic in here
+        switch (operation)
+        {
+            case "+":
+                return first + second;
 
-        return null;
+            case "-":
+                return first - second;
+
+            case "*":
+                return first * second;
+
+            case "/":
+                if (second == 0)
+                {
+                    Console.WriteLine("Cannot divide by zero");
+                    return null;
+                }
+                return Math.Round((double)first / second, 2);
+
+            case "%":
+                if (second == 0)
+                {
+                    Console.WriteLine("Cannot modulo by zero");
+                    return null;
+                }
+                return first % second;
+
+            case "=":
+                return null;
+
+            default:
+                Console.WriteLine("Incorrect Operation Used, please try again");
+                return null;
+        }
     }
 }
-
